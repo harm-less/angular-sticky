8b1b20c0953873ad2adf489e33f6063ce8f907fe
Harm van der Werf <harmvdwerf@gmail.com>
2016-03-12 17:02:12 +0100

    Release v0.0.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -238,7 +238,7 @@ module.exports = function(grunt) {
 	grunt.registerTask('release', function() {
 		var releaseType = grunt.option('releaseType') ? grunt.option('releaseType') : 'patch';
 		grunt.task.run('bump-only:' + releaseType);
-		grunt.config('pkg', grunt.file.readJSON('package.json'));
+		grunt.config.set('pkg', grunt.file.readJSON('package.json'));
 		grunt.task.run('build');
 		grunt.task.run('bump-commit');
 	});
