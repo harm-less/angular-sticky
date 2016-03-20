95a2d251d41718bf974132776bf2ac2066aa6299
Harm van der Werf <harmvdwerf@gmail.com>
2016-03-20 17:41:44 +0100

    Updates

---
+++ scripts/directives/scroll-along.js
@@ -0,0 +1,35 @@
+angular.module('demo.directive.scroll-along', [])
+
+	.directive('hlScrollAlong', function($window) {
+		return {
+			restrict: 'EA',
+			transclude: true,
+			replace: true,
+			template: '<div class="scroll-along-container"><div class="content" ng-transclude></div></div>',
+			link: function(scope, element, attrs) {
+
+				var nativeEl = angular.element(element[0]);
+				var container = angular.element(nativeEl.parent())[0];
+
+				angular.element($window).bind('scroll', render);
+				angular.element($window).bind('mousemove', render);
+
+				function render(event) {
+					var normalised = event.clientY - _getTopOffset(container);
+					nativeEl.css('top', Math.max(0, normalised + window.pageYOffset));
+					scope.$apply();
+				}
+
+				function _getTopOffset(element) {
+					var pixels = 0;
+					if (element && element.offsetParent) {
+						do {
+							pixels += element.offsetTop;
+							element = element.offsetParent;
+						} while (element);
+					}
+					return pixels;
+				}
+			}
+		};
+	});
