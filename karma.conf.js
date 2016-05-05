e80d82c7b9039b3ea56690d370718bf9c4272824
Harm van der Werf <harmvdwerf@gmail.com>
2016-05-05 22:44:55 +0200

    Removed jQuery from the Karma config
    This does however throw some errors when running Karma. These need to be
    fixed if this pull-request will make any sense.

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,6 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'bower_components/jquery/jquery.js',
 			'bower_components/angular/angular.js',
 			'bower_components/angular-mocks/angular-mocks.js',
 			'js/*.js',
